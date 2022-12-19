e9085fa0c82395ee70b7d3e687ad26af0e9d9914
Tackers <83538484+MrTackers@users.noreply.github.com>
2022-12-19 16:12:20 +0100

    Stoel Wijziging
    Stoel Trilt nu op knop indruk
    Stoel Trilt ook op druksensor elke 8 seconden.
    Allebei via de PI

--- stoel/stoel.ino
+++ stoel/stoel.ino
@@ -12,7 +12,12 @@
 const char* ssid     = WIFISSID;
 const char* password = WIFIPASS;
 const char* host = "192.168.4.1";
-const uint16_t port = 8080;
+const uint16_t port = 6969;
+
+unsigned long startMillis;
+unsigned long currentMillis;
+const unsigned long periode = 5000;
+
 
 void setup() 
 { 
@@ -21,6 +26,7 @@ void setup()
   // wifi verbinden enzo
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  startMillis = millis();
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -61,6 +67,15 @@ void TrillenUit(){
   
 }
 
+unsigned int DrukSensor(){
+  Wire.requestFrom(0x36, 4);   
+  unsigned int anin0 = Wire.read()&0x03;  
+  anin0=anin0<<8;
+  anin0 = anin0|Wire.read(); 
+  unsigned int b = anin0; 
+return b;
+}
+
 unsigned int inputs(){
   Wire.beginTransmission(0x38); 
   Wire.write(byte(0x00));      
@@ -92,11 +107,18 @@ void loop() {
   }
 
 
+  TrillenAan();
+  delay(100);
+  TrillenUit();
+
+  delay(5000);
+  Serial.println("Initialisatie compleet");
   while(1){
   Wire.beginTransmission(0x38);
   Wire.write(byte(0x03));          
   Wire.write(byte(0x0F));         
   Wire.endTransmission();
+  
 
   
   while (client.available()) {
@@ -104,9 +126,35 @@ void loop() {
     Serial.print(ch);
   }
 
-  
-  
-  if ((inputs()) > 0 ){
+if ((DrukSensor() > 400)){
+
+      Serial.println(DrukSensor());
+    // Verstuurt een string naar de server.
+    if (client.connected()) {
+      Serial.println("IK HEB DATA VERSTUURD NAAR DE PI");
+      client.print("zitten");
+      delay(100);
+ while (client.available()) {
+        char ch = static_cast<char>(client.read());
+        if (ch == '6'){
+          while(DrukSensor() > 400){
+          delay(8000);
+          Serial.println("IK TRIL NU");
+          TrillenAan();
+          delay(2000);
+          TrillenUit();
+          Serial.println("Tril cyclus compleet begin opniuew");
+
+        }
+        TrillenUit();
+        Serial.println("Patient Opgestaan");
+
+ }
+    }
+}
+  }
+
+    if ((inputs()) > 0 ){
     Serial.println(inputs());
     // Verstuurt een string naar de server.
     if (client.connected()) {
@@ -119,23 +167,16 @@ void loop() {
         char ch = static_cast<char>(client.read());
         if (ch == '5'){
           TrillenAan();
-          Serial.println("IK TRIL NU");
+          Serial.println("IK TRIL NU");          
           delay(2500);
           TrillenUit();
           Serial.println("IK STOP MET TRILLEN");
       }
-        Serial.print(ch);
+        Serial.println(ch);
       }
       
     }
-    //client.stop();
-    //break;    
-    
-  // Close the connection
 
   }
-
-  }
-
 }
-
+}
