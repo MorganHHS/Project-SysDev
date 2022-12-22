dc96dedbefea37d97032e9d6720d8913bafe2599
MikeR20183380 <20183380@student.hhs.nl>
2022-12-22 14:19:45 +0100

    Schemerlamp voortgang
    Bewegingssensor uitlezen (boordje moet worden gereset) led volledig functioneel.
    
    Co-Authored-By: Tackers <83538484+MrTackers@users.noreply.github.com>

---
+++ wemos/schemerlamp/schemerlamp.ino
@@ -0,0 +1,83 @@
+#include <FastLED.h>
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+
+#define NUM_LEDS 1
+#ifndef WIFISSID
+#define WIFISSID "banaantje"         //SSID of the WIFI Accesspoint
+#define WIFIPASS "BananenZijnGoed!"  //Password of the WIFI Accesspoint
+#endif
+
+
+const char* ssid = WIFISSID;
+const char* password = WIFIPASS;
+const char* host = "192.168.4.1";  //Server Host IP
+const uint16_t port = 6969;        // Socket Port number
+
+
+// How many leds in your strip?
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN D5
+
+void ledOn();
+void ledOff();
+
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  // Uncomment/edit one of the following lines for your leds arrangement.
+
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+}
+
+
+unsigned int inputs() {  //Function to read button value
+  Wire.beginTransmission(0x38);
+  Wire.write(byte(0x00));
+  Wire.endTransmission();
+  Wire.requestFrom(0x38, 1);
+  unsigned int a = Wire.read() & 0x01;
+  return a;
+}
+
+void loop(void) {
+
+  ledOn();
+  delay(500);
+  ledOff();
+  delay(500);
+
+  while (1) {
+    Wire.beginTransmission(0x38);
+    Wire.write(byte(0x03));
+    Wire.write(byte(0x0F));
+    Wire.endTransmission();
+
+    Serial.println(inputs());
+
+  Wire.beginTransmission(0x36);
+  Wire.write(byte(0xA2));          
+  Wire.write(byte(0x03));  
+  Wire.endTransmission(); 
+    delay(500);
+  }
+}
+
+void ledOn() {
+
+  leds[0] = CRGB::Red;
+  FastLED.show();
+}
+
+void ledOff() {
+
+  leds[0] = CRGB::Black;
+  FastLED.show();
+}
