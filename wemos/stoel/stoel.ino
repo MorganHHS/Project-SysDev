d21c3c918cdd185e47538287f2fe766fcecf2148
Tackers <83538484+MrTackers@users.noreply.github.com>
2023-01-20 14:40:05 +0100

    Added all test functionality

--- wemos/stoel/stoel.ino
+++ wemos/stoel/stoel.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
+#include <string>
 
 
 #ifndef WIFISSID
@@ -12,11 +13,12 @@
 const char* ssid = WIFISSID;
 const char* password = WIFIPASS;
 const char* host = "192.168.4.1";  //Server Host IP
-const uint16_t port = 6969;        // Socket Port number
+const uint16_t port = 8080;        // Socket Port number
 
 unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long periode = 5000;
+
 
 
 void setup() {
@@ -37,12 +39,28 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
+WiFiClient verbindenPi() {
+  Serial.print("connecting to: ");
+  Serial.print(host);
+  Serial.print(" :");
+  Serial.println(port);
+
+  // Hiermee zet je de Wificlient class in TCP connectie modus.
+  WiFiClient client;  // client aanmaken
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    delay(5000);
+  }
+  return client;
+}
+
 void LedAan() {  //Function to turn led ON
   Wire.beginTransmission(0x38);
   Wire.write(byte(0x01));
   Wire.write(byte(1 << 4));
   Wire.endTransmission();
 }
+
 
 void TrillenAan() {  //Function to turn vibration motor ON
   Wire.beginTransmission(0x38);
@@ -56,6 +74,23 @@ void TrillenUit() {  //Function to turn vibration motor OFF
   Wire.write(byte(0x01));
   Wire.write(byte(0 << 4));
   Wire.endTransmission();
+}
+
+void KnopTril() {
+  TrillenAan();
+  Serial.println("Vibrating");
+  delay(2500);
+  TrillenUit();
+  Serial.println("Stop Vibrating");
+}
+
+void TrilCyclus() {
+  Serial.println("Vibrating");
+  TrillenAan();
+  delay(2000);
+  TrillenUit();
+  delay(8000);
+  Serial.println("Vibration cycle complete");
 }
 
 unsigned int DrukSensor() {  //Function to read force sensor output
@@ -82,27 +117,15 @@ unsigned int inputs() {  //Function to read button value
 void loop() {
 
 
-  //Wifi Connection printing
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
-
-  // Hiermee zet je de Wificlient class in TCP connectie modus.
-  WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    delay(5000);
-    return;
-  }
-
+  WiFiClient client = verbindenPi();
   //Reset vibration outputs
   TrillenAan();
   delay(100);
   TrillenUit();
 
-  delay(5000);
   Serial.println("Initialisatie compleet");
+
+
 
 
   while (1) {
@@ -121,22 +144,15 @@ void loop() {
     }
 
     if ((DrukSensor() > 400)) {  //Start vibration cycle if reasonable force is exerted on force sensor
-      Serial.println(DrukSensor());
-
       if (client.connected()) {  //
         Serial.println("Patient is sitting");
-        client.print("zitten");
+        client.print("stoelTrilCyclus");
         while (!client.available()) {}  //Wait for reaction server
         while (client.available()) {
           char ch = static_cast<char>(client.read());
-          if (ch == '6') {
+          if (ch == '5') {
             while (DrukSensor() > 400) {
-              Serial.println("Vibrating");
-              TrillenAan();
-              delay(2000);
-              TrillenUit();
-              Serial.println("Vibration cycle complete");
-              delay(8000);
+              TrilCyclus();
             }
             TrillenUit();
             Serial.println("Patient stood up");
@@ -146,48 +162,60 @@ void loop() {
     }
 
     if ((inputs()) > 0) {  //Start vibrating if button is pressed
-      Serial.println(inputs());
       // Verstuurt een string naar de server.
       if (client.connected()) {
         Serial.println("Button is pressed");
-        client.print("knop");
+        client.print("stoelKnopTril");
         delay(100);
 
         // Lezen wat de server verstuurd en printen.
         while (client.available()) {
           char ch = static_cast<char>(client.read());
-          if (ch == '5') {
-            TrillenAan();
-            Serial.println("Vibrating");
-            delay(2500);
-            TrillenUit();
-            Serial.println("Stop Vibrating");
+          if (ch == '2') {
+            while ((inputs()) > 0) {
+              KnopTril();
+            }
           }
         }
       }
     }
-    if (client.available()) { // Lezen wat de server verstuurd en printen.
+
+    if (client.available()) {  // Lezen wat de server verstuurd en printen.
       char ch = static_cast<const char>(client.read());
       Serial.print("Pi sent:");
       Serial.print(ch);
       Serial.print("\n");
 
-      if (ch == '7') {
-        TrillenAan();
+      if (ch == '1') {
+        LedAan();
         Serial.println("Vibrating");
       }
-      if (ch == '8') {
-        TrillenUit();
+      if (ch == '2') {
+        KnopTril();
         Serial.println("Stop Vibrating");
       }
-      if (ch == '9') {
-      LedAan();
-      Serial.println("Turn Led on");
+      if (ch == '3') {
+        TrillenAan();
+        Serial.println("Turn Led on");
+      }
+      if (ch == '4') {
+        TrillenUit();
+        Serial.println("Turn Led on");
+      }
+      if (ch == '5') {
+        TrilCyclus();
+        Serial.println("Turn Led on");
       }
       ch = '0';
     }
-
-
-
+    if ((WiFi.status() != WL_CONNECTED)) {
+      Serial.println("WiFI verbinding verbroken");
+      setup();
+      delay(5000);
+    }
+    if (!client.connected()) {
+      Serial.println("Server verbinding verbroken");
+      break;
+    }
   }
 }
